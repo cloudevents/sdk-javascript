268233fce7d8e2902b03650e628b44f194da72dc
Fabio José <fabiojose@gmail.com>
2018-11-09 16:20:29 -0200

    Project start
    Signed-off-by: Fabio José <fabiojose@gmail.com>

---
+++ index.js
@@ -0,0 +1,8 @@
+var Cloudevent = require('./lib/cloudevent.js');
+//var Spec_0_1   = require('./lib/spec_0_1.js');
+//var Spec_0_2   = require('./lib/spec_0_2.js');
+
+module.exports = Cloudevent;
+//module.exports.Spec_0_1   = Spec_0_1;
+//module.exports.Spec_0_2   = Spec_0_2;
+
---
+++ test/cloudevent_spec_0_1.js
@@ -0,0 +1,39 @@
+var expect       = require("chai").expect;
+var Cloudevent   = require("../index.js");
+
+var cloudevent = new Cloudevent()
+                       .type("com.github.pull.create")
+                       .source("urn:event:from:myapi/resourse/123");
+
+describe("CloudEvents Spec 0.1 - JavaScript SDK", () => {
+
+  describe("JSON Format", () => {
+
+    describe("Required context attributes", () => {
+      it("requires 'eventType'", () => {
+        expect(cloudevent.format()).to.have.property('eventType');
+      });
+
+      it("requires 'cloudEventsVersion'", () => {
+        expect(cloudevent.format()).to.have.property('cloudEventsVersion');
+      });
+
+      it("requires 'source'", () => {
+        expect(cloudevent.format()).to.have.property('source');
+      });
+
+      it("requires 'eventID'", () => {
+        expect(cloudevent.format()).to.have.property('eventID');
+      });
+    });
+
+    describe("Backward compatibility", () => {
+      it("should have 'eventTypeVersion'", () => {
+        cloudevent.eventTypeVersion("1.0");
+        expect(cloudevent.format()).to.have.property('eventTypeVersion');
+      });
+    });
+
+  });
+
+});

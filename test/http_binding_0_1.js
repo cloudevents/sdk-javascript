50fdc762f4ed6ac5ceddd243bd7e1ce108cbe964
Fabio José <fabiojose@gmail.com>
2018-11-14 21:18:40 -0200

    Unit testing for http bindings
    Signed-off-by: Fabio José <fabiojose@gmail.com>

---
+++ test/http_binding_0_1.js
@@ -0,0 +1,43 @@
+var expect       = require("chai").expect;
+var Cloudevent   = require("../index.js");
+var nock         = require("nock");
+
+const type        = "com.github.pull.create";
+const source      = "urn:event:from:myapi/resourse/123";
+const webhook     = "https://cloudevents.io/webhook";
+const contentType = "application/cloudevents+json; charset=utf-8";
+
+var cloudevent = new Cloudevent()
+                       .type(type)
+                       .source(source);
+
+var httpcfg = {
+  method : 'POST',
+  url    : webhook + '/json'
+};
+
+var httpstructured_0_1 = 
+  new Cloudevent.bindings['http-structured0.1'](httpcfg);
+
+describe("HTTP Transport Binding", () => {
+  beforeEach(() => {
+    // Mocking the webhook
+    nock(webhook)
+      .post("/json")
+      .reply(201, {status: 'accepted'});
+  });
+
+  describe("Structured", () => {
+    describe("JSON Format", () => {
+      it("requires '" + contentType + "' Content-Type in header", () => {
+        return httpstructured_0_1.emit(cloudevent)
+          .then(response => {
+            //console.log(response.config);
+            expect(response.config.headers['Content-Type'])
+              .to.equal(contentType);
+          });
+      });
+    });
+  });
+});
+

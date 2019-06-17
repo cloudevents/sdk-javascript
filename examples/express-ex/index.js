d21881a2b58b66dd2e6723d8ba80ae9b57bc89ad
Fabio José <fabiojose@gmail.com>
2019-06-16 21:10:26 -0300

    WIP: example with expressjs
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- examples/express-ex/index.js
+++ examples/express-ex/index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+  console.log(req.headers);
+  console.log(req.body);
+
+  // TODO use the Unmarshaller
+  
+  res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
-

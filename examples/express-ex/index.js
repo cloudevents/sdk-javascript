5baa52dd2b5da12c3cd6e8f72e2209b5525f8278
Fabio José <fabiojose@gmail.com>
2019-06-21 16:43:25 -0300

    Fix example body read
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- examples/express-ex/index.js
+++ examples/express-ex/index.js
@@ -1,13 +1,39 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', function (req, res) {
+const Unmarshaller02 = require("cloudevents-sdk/http/unmarshaller/v02");
+var unmarshaller = new Unmarshaller02();
+
+app.use((req, res, next) => {
+    var data='';
+
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) {
+       data += chunk;
+    });
+
+    req.on('end', function() {
+        req.body = data;
+        next();
+    });
+});
+
+app.post('/', function (req, res) {
   console.log(req.headers);
   console.log(req.body);
 
-  // TODO use the Unmarshaller
-  
-  res.send('Hello World!');
+  try {
+    var event = unmarshaller.unmarshall(req.body, req.headers);
+
+    res.status(201)
+          .send("Event Accepted");
+  }catch(e) {
+    console.error(e);
+
+    res.status(400)
+          .header("Content-Type", "application/json")
+          .send(JSON.stringify(e));
+  }
 });
 
 app.listen(3000, function () {

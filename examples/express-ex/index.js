b39a32152fff669b8f5c608cca8ad528cf1e19d7
Fabio José <fabiojose@gmail.com>
2019-06-25 11:02:14 -0300

    Double quotes
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- examples/express-ex/index.js
+++ examples/express-ex/index.js
@@ -41,5 +41,5 @@ app.post('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });

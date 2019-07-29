6b5a8d1f3eb96ce8af9c4ef3e4ea68d050265465
Fabio José <fabiojose@gmail.com>
2019-07-29 14:46:31 -0300

    should have the minimum exports
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- test/sdk_test.js
+++ test/sdk_test.js
@@ -7,8 +7,24 @@ describe("The SDK Requirements", () => {
     it("should create an event using the right spec version", () => {
       expect(v02.event().spec.payload.specversion).to.equal("0.2");
     });
+
+    it("should have 'Spec' export", () => {
+      expect(v02).to.have.property("Spec");
+    });
+
+    it("should have 'StructuredHTTPEmitter' export", () => {
+      expect(v02).to.have.property("StructuredHTTPEmitter");
+    });
+
+    it("should have 'BinaryHTTPEmitter' export", () => {
+      expect(v02).to.have.property("BinaryHTTPEmitter");
+    });
+
+    it("should have 'event' export", () => {
+      expect(v02).to.have.property("event");
+    });
   });
-  
+
   describe("v0.3", () => {
     it("should create an event using the right spec version", () => {
       expect(v03.event().spec.payload.specversion).to.equal("0.3");

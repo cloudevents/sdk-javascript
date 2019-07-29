8598be327b4e13ad7d946f0b5536215b313569f0
Fabio José <fabiojose@gmail.com>
2019-07-29 13:19:54 -0300

    Export the SDK minimum requirements
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- v02/index.js
+++ v02/index.js
@@ -1,5 +1,16 @@
+const Cloudevent = require("../lib/cloudevent.js");
 const Spec = require("../lib/specs/spec_0_2.js");
+const StructuredHTTPEmitter =
+  require("../lib/bindings/http/emitter_structured_0_2.js");
+const {HTTPBinary02} = require("../lib/bindings/http/emitter_binary_0_2.js");
+
+function event() {
+  return new Cloudevent(Spec);
+}
 
 module.exports = {
-  Spec
+  Spec,
+  StructuredHTTPEmitter,
+  BinaryHTTPEmitter : HTTPBinary02,
+  event
 };

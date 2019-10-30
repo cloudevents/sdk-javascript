f47de94b4886d13134c40c7d9ed85d3a92ef179f
Fabio José <fabiojose@gmail.com>
2019-10-29 21:15:29 -0300

    Exporting the http binary emitter spec 1.0
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- v1/index.js
+++ v1/index.js
@@ -2,6 +2,7 @@ const Cloudevent = require("../lib/cloudevent.js");
 const Spec = require("../lib/specs/spec_1.js");
 const StructuredHTTPEmitter =
   require("../lib/bindings/http/emitter_structured.js");
+const BinaryHTTPEmitter = require("../lib/bindings/http/emitter_binary_1.js");
 
 function event() {
   return new Cloudevent(Spec);
@@ -10,5 +11,6 @@ function event() {
 module.exports = {
   Spec,
   StructuredHTTPEmitter,
+  BinaryHTTPEmitter,
   event
 };

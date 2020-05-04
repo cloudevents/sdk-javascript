5b747eea85616862834208abcb730aabb1057119
Lance Ball <lball@redhat.com>
2020-05-04 12:48:47 -0400

    fix: make application/json the default content type in binary mode
    The Knative Kafka event source does not include a `Content-Type` header when
    sending binary events. The CE HTTP binding specification doesn't address how
    a receiver should handle this situation.
    
    This commit makes `application/json` the default.
    
    Fixes: https://github.com/cloudevents/sdk-javascript/issues/117
    Ref: https://github.com/cloudevents/spec/issues/614
    
    Signed-off-by: Lance Ball <lball@redhat.com>

--- test/bindings/http/receiver_binary_0_3_tests.js
+++ test/bindings/http/receiver_binary_0_3_tests.js
@@ -144,6 +144,20 @@ describe("HTTP Transport Binding Binary Receiver for CloudEvents v0.3", () => {
       expect(receiver.check.bind(receiver, payload, attributes))
         .to.not.throw();
     });
+
+    it("No error when content-type is unspecified", () => {
+      const payload = {};
+      const attributes = {
+        "ce-type": "type",
+        "ce-specversion": "0.3",
+        "ce-source": "source",
+        "ce-id": "id"
+      };
+
+      // act and assert
+      expect(receiver.check.bind(receiver, payload, attributes))
+        .to.not.throw();
+    });
   });
 
   describe("Parse", () => {

ff5d96548176720acc56480aee18c1b4043081c0
Lance Ball <lball@redhat.com>
2020-05-08 12:09:13 -0400

    docs: update README and examples with new API
    This commit modifies the README to show new API usage for the
    `HTTPReceiver` and `CloudEvent` classes, and updates the examples
    to use this as well.
    
    Overall structure and content has been modified to look more like
    the sdk-go README.
    
    Fixes: https://github.com/cloudevents/sdk-javascript/issues/128
    
    Signed-off-by: Lance Ball <lball@redhat.com>

--- examples/express-ex/index.js
+++ examples/express-ex/index.js
@@ -1,14 +1,10 @@
 /* eslint-disable no-console */
 
 const express = require("express");
+const { HTTPReceiver } = require("../../");
+
 const app = express();
-
-const v03 = require("cloudevents-sdk/v03");
-const unmarshaller03 = new v03.HTTPUnmarshaller();
-
-const v1 = require("cloudevents-sdk/v1");
-const structured1 = new v1.StructuredHTTPReceiver();
-const binary1 = new v1.BinaryHTTPReceiver();
+const receiver = new HTTPReceiver();
 
 app.use((req, res, next) => {
   let data = "";
@@ -24,65 +20,21 @@ app.use((req, res, next) => {
   });
 });
 
-app.post("/v1", function(req, res) {
+app.post("/", function(req, res) {
   console.log(req.headers);
   console.log(req.body);
 
   try {
-    const myevent = structured1.parse(req.body, req.headers);
-    // pretty print
-    console.log("Accepted event:");
-    console.log(JSON.stringify(myevent.format(), null, 2));
-
-    res.status(201)
-      .json(myevent.format());
+    const event = receiver.accept(req.headers, req.body);
+    const asJSON = event.format();
+    console.log(`Accepted event: ${JSON.stringify(event.format(), null, 2)}`);
+    res.status(201).json(asJSON);
   } catch (err) {
     console.error(err);
     res.status(415)
       .header("Content-Type", "application/json")
       .send(JSON.stringify(err));
   }
-});
-
-app.post("/v1/binary", function(req, res) {
-  console.log(req.headers);
-  console.log(req.body);
-
-  try {
-    const myevent = binary1.parse(req.body, req.headers);
-    // pretty print
-    console.log("Accepted event:");
-    console.log(JSON.stringify(myevent.format(), null, 2));
-
-    res.status(201)
-      .json(myevent.format());
-  } catch (err) {
-    console.error(err);
-    res.status(415)
-      .header("Content-Type", "application/json")
-      .send(JSON.stringify(err));
-  }
-});
-
-app.post("/v03", function(req, res) {
-  console.log(req.headers);
-  console.log(req.body);
-
-  unmarshaller03.unmarshall(req.body, req.headers)
-    .then((cloudevent) => {
-      // pretty print
-      console.log("Accepted event:");
-      console.log(JSON.stringify(cloudevent.format(), null, 2));
-
-      res.status(201)
-        .json(cloudevent.format());
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(415)
-        .header("Content-Type", "application/json")
-        .send(JSON.stringify(err));
-    });
 });
 
 app.listen(3000, function() {

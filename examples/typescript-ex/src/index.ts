dcb3c4e98acb5fc4fca518d2121a46797388456f
Lucas Holmquist <lholmqui@redhat.com>
2020-06-09 18:08:15 -0400

    chore: Update examples to use the latest sdk version(2.0.2) (#206)
    Signed-off-by: Lucas Holmquist <lholmqui@redhat.com>

--- examples/typescript-ex/src/index.ts
+++ examples/typescript-ex/src/index.ts
@@ -1,45 +1,49 @@
-import { CloudEvent, HTTPREceiver } from '../../';
+import { CloudEvent, HTTPReceiver } from "cloudevents-sdk";
+import { CloudEventV1 } from "cloudevents-sdk/lib/v1";
 
 export function doSomeStuff() {
-  const receiver = new HTTPREceiver();
+  const receiver = new HTTPReceiver();
 
-  const myevent: CloudEvent = new CloudEvent()
-    .source('/source')
-    .type('type')
-    .dataContentType('text/plain')
-    .dataschema('http://d.schema.com/my.json')
-    .subject('cha.json')
-    .data('my-data')
-    .addExtension("my-ext", "0x600");
+  const myevent: CloudEventV1 = new CloudEvent({
+    source: "/source",
+    type: "type",
+    dataContentType: "text/plain",
+    dataSchema: "https://d.schema.com/my.json",
+    subject: "cha.json",
+    data: "my-data"
+  });
+  myevent.addExtension("extension-1", "some extension data");
 
-  console.log(myevent.toString());
-  console.log(myevent.getExtensions());
+  console.log("My structured event:", myevent.toString());
+  console.log("My structured event extensions:", myevent.getExtensions());
 
   // ------ receiver structured
-  const payload = myevent.toString();
+  // The header names should be standarized to use lowercase
   const headers = {
-    "Content-Type":"application/cloudevents+json"
+    "content-type": "application/cloudevents+json"
   };
 
-  console.log(receiver.accept(headers, payload).toString());
+  // Typically used with an incoming HTTP request where myevent.format() is the actual
+  // body of the HTTP
+  console.log("Received structured event:", receiver.accept(headers, myevent.format()).toString());
 
   // ------ receiver binary
-  const extension1 = "mycuston-ext1";
   const data = {
-    "data" : "dataString"
+    "data": "dataString"
   };
   const attributes = {
-    "ce-type"        : "type",
-    "ce-specversion" : "1.0",
-    "ce-source"      : "source",
-    "ce-id"          : "id",
-    "ce-time"        : "2019-06-16T11:42:00Z",
-    "ce-dataschema"  : "http://schema.registry/v1",
-    "Content-Type"   : "application/json",
-    "ce-extension1"  : extension1
+    "ce-type": "type",
+    "ce-specversion": "1.0",
+    "ce-source": "source",
+    "ce-id": "id",
+    "ce-time": "2019-06-16T11:42:00Z",
+    "ce-dataschema": "http://schema.registry/v1",
+    "Content-Type": "application/json",
+    "ce-extension1": "extension1"
   };
 
-  console.log(receiver.accept(attributes, data).toString());
+  console.log("My binary event:", receiver.accept(attributes, data).toString());
+  console.log("My binary event extensions:", receiver.accept(attributes, data).toString());
 
   return true;
 }

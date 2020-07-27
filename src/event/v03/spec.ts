d67d181d776216da6052ab5b204ea4fd812fb9c1
Lance Ball <lball@redhat.com>
2020-07-27 12:11:43 -0400

    lib: remove unused code
    This commit removes two functions that are not actually used.

--- src/event/v03/spec.ts
+++ src/event/v03/spec.ts
@@ -1,23 +1,11 @@
-import { v4 as uuidv4 } from "uuid";
 import Ajv from "ajv";
 import { ValidationError, isBase64 } from "../validation";
-import { CloudEventV03, CloudEventV03Attributes } from "./cloudevent";
-import { CloudEvent } from "../..";
+import { CloudEventV03 } from "./cloudevent";
 import { schema } from "./schema";
 import CONSTANTS from "../../constants";
 
 const ajv = new Ajv({ extendRefs: true });
 const isValidAgainstSchema = ajv.compile(schema);
-
-export function createV03(attributes: CloudEventV03Attributes): CloudEventV03 {
-  const event: CloudEventV03 = {
-    specversion: schema.definitions.specversion.const,
-    id: uuidv4(),
-    time: new Date().toISOString(),
-    ...attributes,
-  };
-  return new CloudEvent(event);
-}
 
 export function validateV03(event: CloudEventV03): boolean {
   if (!isValidAgainstSchema(event)) {
--- src/event/v1/spec.ts
+++ src/event/v1/spec.ts
@@ -1,24 +1,11 @@
 import Ajv from "ajv";
-import { v4 as uuidv4 } from "uuid";
-
-import { CloudEvent } from "../cloudevent";
-import { CloudEventV1, CloudEventV1Attributes } from "./cloudevent";
+import { CloudEventV1 } from "./cloudevent";
 import { ValidationError } from "../validation";
 
 import { schemaV1 } from "./schema";
 
 const ajv = new Ajv({ extendRefs: true });
 const isValidAgainstSchema = ajv.compile(schemaV1);
-
-export function createV1(attributes: CloudEventV1Attributes): CloudEventV1 {
-  const event: CloudEventV1 = {
-    specversion: schemaV1.definitions.specversion.const,
-    id: uuidv4(),
-    time: new Date().toISOString(),
-    ...attributes,
-  };
-  return new CloudEvent(event);
-}
 
 export function validateV1(event: CloudEventV1): boolean {
   if (!isValidAgainstSchema(event)) {

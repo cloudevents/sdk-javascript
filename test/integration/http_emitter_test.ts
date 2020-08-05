e8c890d361742b0e73442ffe4d80fefd9d774356
Lucas Holmquist <lholmqui@redhat.com>
2020-08-05 18:21:23 -0400

    squash: update test to fail
    Signed-off-by: Lucas Holmquist <lholmqui@redhat.com>

--- test/integration/http_emitter_test.ts
+++ test/integration/http_emitter_test.ts
@@ -4,6 +4,7 @@ import nock from "nock";
 import CONSTANTS from "../../src/constants";
 
 const DEFAULT_CE_CONTENT_TYPE = CONSTANTS.DEFAULT_CE_CONTENT_TYPE;
+const DEFAULT_CONTENT_TYPE = CONSTANTS.DEFAULT_CONTENT_TYPE;
 
 import { CloudEvent, Version, Emitter, Protocol, headersFor } from "../../src";
 import { AxiosResponse } from "axios";
@@ -55,6 +56,7 @@ describe("HTTP Transport Binding Emitter for CloudEvents", () => {
         .send(event)
         .then((response: AxiosResponse) => {
           // A binary message will have a ce-id header
+          expect(response.data["content-type"]).to.equal(DEFAULT_CONTENT_TYPE);
           expect(response.data[CONSTANTS.CE_HEADERS.ID]).to.equal(event.id);
           expect(response.data[CONSTANTS.CE_HEADERS.SPEC_VERSION]).to.equal(Version.V1);
           // A binary message will have a request body for the data

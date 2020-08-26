e2eb4766d355c767fd3d09fcdedb258b71647135
Lance Ball <lball@redhat.com>
2020-08-26 18:26:50 -0400

    feat: introduce Message, Serializer, Deserializer and Binding interfaces (#324)
    * lib(messages): Implement a 4.0 Messages and other supporting interfaces
    
    This commit introduces the Message, Serializer and Deserializer, and Binding
    interfaces used to convert a CloudEvent into a Message that can be sent across
    a transport protocol. The first protocol implemented for this is HTTP, and some
    of the functionality formerly in src/transport/http has been simplified,
    reduced and/or moved to /src/messages/http.
    
    Test for V1 and V3 events are in place. Conformance tests have been modified to use
    these new interfaces vs. the HTTP Receiver class.
    
    Signed-off-by: Lance Ball <lball@redhat.com>

---
+++ src/message/index.ts
@@ -0,0 +1,75 @@
+import { CloudEvent } from "..";
+import { binary, deserialize, structured, isEvent } from "./http";
+import { headersFor } from "./http/headers";
+
+/**
+ * Binding is an interface for transport protocols to implement,
+ * which provides functions for sending CloudEvent Messages over
+ * the wire.
+ */
+export interface Binding {
+  binary: Serializer;
+  structured: Serializer;
+  toEvent: Deserializer;
+  isEvent: Detector;
+}
+
+/**
+ * Headers is an interface representing transport-agnostic headers as
+ * key/value string pairs
+ */
+export interface Headers {
+  [key: string]: string;
+}
+
+/**
+ * Message is an interface representing a CloudEvent as a
+ * transport-agnostic message
+ */
+export interface Message {
+  headers: Headers;
+  body: string;
+}
+
+/**
+ * An enum representing the two transport modes, binary and structured
+ */
+export enum Mode {
+  BINARY = "binary",
+  STRUCTURED = "structured",
+}
+
+/**
+ * Serializer is an interface for functions that can convert a
+ * CloudEvent into a Message.
+ */
+export interface Serializer {
+  (event: CloudEvent): Message;
+}
+
+/**
+ * Deserializer is a function interface that converts a
+ * Message to a CloudEvent
+ */
+export interface Deserializer {
+  (message: Message): CloudEvent;
+}
+
+/**
+ * Detector is a function interface that detects whether
+ * a message contains a valid CloudEvent
+ */
+export interface Detector {
+  (message: Message): boolean;
+}
+
+// HTTP Message capabilities
+export const HTTP: Binding = {
+  binary: binary as Serializer,
+  structured: structured as Serializer,
+  toEvent: deserialize as Deserializer,
+  isEvent: isEvent as Detector,
+};
+
+// TODO: Deprecated. Remove this for 4.0
+export { headersFor };

4779d89ad054973cb0af8ec4119e073b78970392
Philip Hayes <philip@deewhy.ie>
2020-09-02 17:09:53 -0400

    chore(example): Replaced body parser with express JSON parser (#334)
    Signed-off-by: Philip Hayes <phayes@redhat.com>
    
    Co-authored-by: Philip Hayes <phayes@redhat.com>

--- examples/express-ex/index.js
+++ examples/express-ex/index.js
@@ -3,20 +3,8 @@
 const express = require("express");
 const { Receiver } = require("cloudevents");
 const app = express();
-
-app.use((req, res, next) => {
-  let data = "";
-
-  req.setEncoding("utf8");
-  req.on("data", function (chunk) {
-    data += chunk;
-  });
-
-  req.on("end", function () {
-    req.body = data;
-    next();
-  });
-});
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
 app.post("/", (req, res) => {
   console.log("HEADERS", req.headers);

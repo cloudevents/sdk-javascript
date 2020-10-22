f30a366db159e0cf077f53d16884fe2cd4663172
Remi Cattiau <remi@cattiau.com>
2020-10-22 11:22:48 -0700

    feat: add other interfaces from the spec for discovery
    Signed-off-by: Remi Cattiau <remi@cattiau.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { CloudEvent, Version } from "./event/cloudevent";
 import { ValidationError } from "./event/validation";
-import { CloudEventV03, CloudEventV03Attributes, CloudEventV1, CloudEventV1Attributes } from "./event/interfaces";
+export * from "./event/interfaces";
 
 import {
   Emitter,
@@ -19,10 +19,6 @@ import CONSTANTS from "./constants";
 export {
   // From event
   CloudEvent,
-  CloudEventV03,
-  CloudEventV03Attributes,
-  CloudEventV1,
-  CloudEventV1Attributes,
   Version,
   ValidationError,
   // From message

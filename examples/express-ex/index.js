5877d99f5883a0b467848f7b0d4532ddd04a9d06
Remi Cattiau <remi@cattiau.com>
2020-10-22 12:15:46 -0700

    docs: split discovery example from receiver example
    Signed-off-by: Remi Cattiau <remi@cattiau.com>

--- examples/express-ex/index.js
+++ examples/express-ex/index.js
@@ -1,49 +1,10 @@
 /* eslint-disable */
 
 const express = require("express");
-const { Receiver, DiscoveryService } = require("cloudevents");
+const { Receiver } = require("cloudevents");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
-DiscoveryService.registerService({
-  url: "https://example.com/services/widgetService",
-  name: "com.example.services.widgetService",
-  specversions: ["1.0"],
-  subscriptionurl: "https://events.example.com",
-  protocols: ["HTTP"],
-  types: [
-    {
-      type: "com.example.widget.create",
-      type: "com.example.widget.delete",
-    },
-  ],
-});
-DiscoveryService.registerService({
-  url: "https://example.com/services/catService",
-  name: "com.example.services.catService",
-  specversions: ["1.0"],
-  subscriptionurl: "https://cats.example.com",
-  protocols: ["HTTP"],
-  types: [
-    {
-      type: "com.example.cats.buy",
-      type: "com.example.cats.adopt",
-      type: "com.example.cats.feed",
-      type: "com.example.cats.give",
-      // Just to play with discovery service
-      type: "com.example.widget.create",
-    },
-  ],
-});
-
-DiscoveryService.express(app);
-// Create a anonymous mapping of cloud events for example
-DiscoveryService.express(app, "/anonymous", (name, type, req) => {
-  // A true life example would have only one call to express and check req for permission
-  // Only give access to the catService and widgetCreate if anonymous
-  return name === "com.example.widget.create" || name === "com.example.services.catService";
-});
 
 app.post("/", (req, res) => {
   console.log("HEADERS", req.headers);

7da6533da6f67bb0de8d27b113eadc146c3e584b
Grant Timmerman <timmerman+devrel@google.com>
2021-09-29 12:29:35 -0700

    ci: revert symbol change
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

--- test/integration/cloud_event_test.ts
+++ test/integration/cloud_event_test.ts
@@ -57,7 +57,7 @@ describe("A CloudEvent", () => {
 
   it("serializes as JSON with raw log", () => {
     const ce = new CloudEvent({ ...fixture, data: { lunch: "tacos" } });
-    expect(ce.toString()).to.deep.equal(ce[Symbol.for("nodejs.util.inspect.custom")]());
+    expect(ce.toString()).to.deep.equal(JSON.stringify(ce));
   });
 
   it("Throw a validation error for invalid extension names", () => {

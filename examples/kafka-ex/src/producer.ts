82dec53b331a5b500de4c2af1c4b4b62ee15d789
Femi <alayesanmifemi@gmail.com>
2025-09-20 20:12:43 +0100

    feat(kafka-sample): added code sample for kafka

---
+++ examples/kafka-ex/src/producer.ts
@@ -0,0 +1,40 @@
+/* eslint-disable */
+import { CloudEvent, Kafka } from "cloudevents";
+import readline from "readline";
+import kafka from "./client";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+(async () => {
+  const producer = kafka.producer();
+  await producer.connect();
+
+  rl.setPrompt("> ");
+  rl.prompt();
+  rl.on("line", async (line) => {
+    const event = new CloudEvent({
+      source: "cloudevents-producer",
+      type: "events.cloudevents.test",
+      datacontenttype: "text/plain",
+      partitionkey: "1",
+      data: line,
+    });
+
+    const message = Kafka.structured(event);
+    
+    await producer.send({
+      topic: "events.cloudevents.test",
+      messages: [
+        message
+      ],
+    });
+    rl.prompt();
+  });
+
+  rl.on("close", async () => {
+    await producer.disconnect();
+  });
+})();
